bb702f05cefd99cf8662580689f11fb8c5dd30ed
Felipe Crovesy <crovesy11@gmail.com>
2025-02-28 12:37:30 -0300

    feat: organize entry point

--- WebCrawler.Presentation/EntryPoint.cs
+++ WebCrawler.Presentation/EntryPoint.cs
@@ -3,33 +3,46 @@ using WebCrawler.Core.UseCases.CrawlProxies;
 
 namespace WebCrawler.Presentation;
 
-public class EntryPoint
+public static class EntryPoint
 {
     public static async Task UserInteraction(CrawlProxies crawlProxies, CrawlProducts crawlProducts)
     {
-        DrawMenu(["1 - CrawlProxies", "2 - CrawlProducts"]);
-
-        var option = Console.ReadLine();
-
-        switch (option)
+        while (true)
         {
-            case "1":
-                await crawlProxies.Handle();
-                break;
-            case "2":
-                await crawlProducts.Handle();
-                break;
-            default:
-                Console.WriteLine("Opção inválida.");
-                break;
+            DrawMenu(["1 - Crawl Proxies", " 2 - Crawl Products"]);
+
+            Console.Write("Escolha o item que deseja coletar: ");
+            var option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    await crawlProxies.Handle();
+                    break;
+                case "2":
+                    await crawlProducts.Handle();
+                    break;
+                case "exit":
+                    return;
+                default:
+                    Console.WriteLine("Opção inválida.");
+                    break;
+            }
+
+            Console.WriteLine("\nPressione Enter para continuar...");
+            Console.ReadLine();
+            Console.Clear();
         }
     }
 
     private static void DrawMenu(string[] options)
     {
-        var maxWidth = options.Max(option => option.Length) + 4;
+        var title = "Welcome to Crawler!";
+
+        var maxWidth = Math.Max(title.Length, options.Max(option => option.Length)) + 4;
 
         DrawTopLines(maxWidth);
+        DrawCenteredText(title, maxWidth);
         DrawOptions(options, maxWidth);
         DrawBottomLines(maxWidth);
     }
@@ -48,10 +61,16 @@ public class EntryPoint
     {
         foreach (var option in options)
         {
-            var leftPadding = (width - option.Length - 2) / 2;
-            var rightPadding = width - option.Length - 2 - leftPadding;
-
-            Console.WriteLine("|" + new string(' ', leftPadding) + option + new string(' ', rightPadding) + "|");
+            DrawCenteredText(option, width);
         }
+    }
+
+    private static void DrawCenteredText(string text, int width)
+    {
+        var padding = width - text.Length - 2;
+        var leftPadding = padding / 2;
+        var rightPadding = padding - leftPadding;
+
+        Console.WriteLine("|" + new string(' ', leftPadding) + text + new string(' ', rightPadding) + "|");
     }
 }
